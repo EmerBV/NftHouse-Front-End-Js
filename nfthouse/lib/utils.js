9a053fb0a85154d60a59734fe78b31a662876ad1
Emerson BV <emersmc@hotmail.com>
2022-02-03 19:11:56 +0100

    first commit

---
+++ nfthouse/lib/utils.js
@@ -0,0 +1,14 @@
+
+function isAPIRequest (req) {
+  return req.originalUrl.startsWith('/api/')
+}
+/** 
+function imgRoute (req, image) {
+  return `http://${req.get('host')}/img/${image}`
+}
+*/
+
+module.exports = {
+  isAPIRequest,
+  //imgRoute
+}
